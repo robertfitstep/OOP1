c58d1eab27ede1546cb08bf1821dc1e99bf6edc6
CSharpFri <CSharpFri@CLASS-2-01>
2017-11-03 18:53:30 +0100

    Init commit

---
+++ ConsoleApp1/Dom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Dom
+    {
+        private string nazov, typ, poschodie ;
+        private int obytneMiestnosti ;
+        private float uzitkovaPlocha, zastavanaPlocha ;
+        public Dom(string nazov, string typ, string poschodie, int obytneMiestnosti, float uzitkovaPlocha, float zastavanaPlocha)
+        {
+            this.nazov = nazov;
+            this.typ = typ;
+            this.poschodie = poschodie;
+            this.obytneMiestnosti = obytneMiestnosti;
+            this.uzitkovaPlocha = uzitkovaPlocha;
+            this.zastavanaPlocha = zastavanaPlocha;
+        }
+    }
+}
